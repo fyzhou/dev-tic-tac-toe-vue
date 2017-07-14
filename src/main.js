f80ee37a87dceeef8b15eff1085482fa5d9df26d
Franklin Zhou <franklinzhouucsd@gmail.com>
2017-07-14 00:50:06 -0700

    Moving Dev Environment to Windows
    Using a new spacing plugin for VSC

--- src/main.js
+++ src/main.js
@@ -15,13 +15,13 @@ Vue.use(VueRouter);
 Firebase.initializeApp(firebaseconfig);
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+    mode: 'history',
+    routes
 });
 
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 })
